f5a0f9720a160d399bcd3f4347a65ece5ff8358f
ychinata <ychinata@qq.com>
2023-06-17 13:00:21 +0800

    No.: 3_1_balancecar Author:xy Description:封装usart3.c的引脚

--- proj_3_balancecar/proj_3_1_balancecar_wheeltec/MiniBalance_HARDWARE/USART3/usart3.h
+++ proj_3_balancecar/proj_3_1_balancecar_wheeltec/MiniBalance_HARDWARE/USART3/usart3.h
@@ -20,6 +20,14 @@ All rights reserved
 #ifndef __USRAT3_H
 #define __USRAT3_H 
 #include "sys.h"	  	
+
+//定义蓝牙串口驱动管脚
+#define BT_IN_RCC    RCC_APB2Periph_GPIOB
+#define BT_IN_GPIO      GPIOB    
+#define BT_PIN_TX  GPIO_Pin_10      
+#define BT_PIN_RX  GPIO_Pin_11
+//定义蓝牙串口驱动管脚
+
 extern u8 Usart3_Receive;
 void uart3_init(u32 bound);
 void USART3_IRQHandler(void);
